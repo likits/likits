bd2d8453d23c8fb565e173364f1ef4811cd72f90
Kaliyaeden <kaliyaeden@gmail.com>
2016-06-16 00:26:26 +0800

    添加展示区->待推送界面。

--- WebContent/assets/admin/js/show.js
+++ WebContent/assets/admin/js/show.js
@@ -1,31 +1,6 @@
 $(document).ready(function(){
 
-    //Set tabs
-    $('#tabsMenu').tabs({
-        onSelect:function(title,index){
-            if(index == 0){
-                showDatagrid('#check');
-            }else if(index == 1){
-                showDatagrid('#push');
-            }else if(index ==2){
-                showDatagrid('#pushed');
-            }
-        }
-    });
 });
-
-function showDatagrid(tabsId){
-    $(tabsId).datagrid({
-        url:'datagrid_data.json',
-        columns:[[
-            {field:'code',title:'Code',width:100},
-            {field:'name',title:'Name',width:100},
-            {field:'price',title:'Price',width:100,align:'right'}
-        ]]
-    });
-}
-
-
 
 var url;
 
@@ -39,26 +14,17 @@ function normalCheck(){
 }
 
 function multipleCheck(){
-
+    var row = $('#check').datagrid('getSelected');
+    if (row){
+        $('#check-dlg').dialog('open').dialog('center').dialog('setTitle','待审核');
+        $('#fm').form('load',row);
+        url = 'update_user.php?id='+row.id;
+    }
 }
 
-function saveUser(){
-    $('#fm').form('submit',{
-        url: url,
-        onSubmit: function(){
-            return $(this).form('validate');
-        },
-        success: function(result){
-            var result = eval('('+result+')');
-            if (result.errorMsg){
-                $.messager.show({
-                    title: 'Error',
-                    msg: result.errorMsg
-                });
-            } else {
-                $('#dlg').dialog('close');        // close the dialog
-                $('#check').datagrid('reload');    // reload the user data
-            }
-        }
-    });
+function checkPass(){
+    var row = $('#check').datagrid('getSelected');
+    if (row){
+        $('#check-dlg').dialog('close');
+    }
 }
